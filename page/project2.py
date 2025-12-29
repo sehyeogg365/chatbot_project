a29c97f997188f06b32a231694dfc0c340adafd7
DESKTOP-9H85EJE\USER <lsh27572757@gmail.com>
2025-12-29 16:24:05 +0900

    docs: 시스템 아키텍처.png, 기획서.pdf 수정 feat: 시스템 아키텍처 추가

--- page/project2.py
+++ page/project2.py
@@ -6,5 +6,8 @@ def app():
 	col1, col2, col3 = st.columns([1, 2, 1]) # 비율 설정 (좌, 중, 우)
 
 	with col2: # 가운데 컬럼에 이미지 배치
+		st.title("시스템 아키텍처")
+		st.image("docs/시스템 아키텍처.png", caption="시스템 아키텍처", use_container_width=True, width=300)
+		st.title("DataFlow")
 		st.image("docs/DataFlow.png", caption="DataFlow", use_container_width=True, width=300)
 	p2d.desc()
